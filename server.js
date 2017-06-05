abb00f47dc578002a766619a10e076804189f7b7
Jack Mooney <mooneyj94@gmail.com>
2017-06-05 20:14:11 +0000

    finished timestamp api

--- server.js
+++ server.js
@@ -1,8 +1,22 @@
 const express = require('express')
 const app = express()
+var timestamp = {}
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
+app.get('/:time', function (req, res) {
+  var time = req.params.time
+  var isnum = /^\d+$/.test(time);
+  if(isnum) var date = new Date(+time * 1000)
+  else var date = new Date(time)
+  if(isNaN( date ))
+  {
+    timestamp.unix = null
+    timestamp.natural = null
+  }
+  else{
+    timestamp.unix = date.getTime() / 1000
+    timestamp.natural = date.toString()
+  }
+  res.json(timestamp)
 })
 
 app.listen(8080, function () {
